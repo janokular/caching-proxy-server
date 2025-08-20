61fbf1ff07b065f21760207a3d393574752018f9
janokular <janekokularczyk@gmail.com>
2025-08-20 11:36:26 +0200

    updated origin_validator.py

--- utils/origin_validator.py
+++ utils/origin_validator.py
@@ -5,9 +5,8 @@ ORIGIN_PATTERN = r'^(https?):\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$'
 
 
 def is_origin_valid(origin: str):
-    '''Validate origin URL structure'''
     if bool(re.search(ORIGIN_PATTERN, origin)):
         return True
     else:
-        print(f'error: "{origin}" is invalid')
+        print(f'error: {origin} is invalid')
         return False

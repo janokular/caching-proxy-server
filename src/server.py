97d7175577cbd73979cd5b3d6d51e3c77484f4e5
janokular <janekokularczyk@gmail.com>
2025-08-21 12:42:36 +0200

    updated server.py

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from http.client import HTTPConnection
+from http.client import HTTPConnection, InvalidURL
 from urllib.parse import urlparse, unquote, quote
 
 
@@ -28,15 +28,39 @@ def create_handler(origin: str):
 
 def handle_request(request_handler: SimpleHTTPRequestHandler, origin: str):
     url_path = unquote(request_handler.path)
-    cache_file_path = get_cache_file_path(url_path)
+    cache_path = get_cache_file_path(url_path)
 
-    if os.path.exists(cache_file_path):
-        print(f'X-Cache hit: {url_path} - fetched from cache')
-        with open(cache_file_path, 'rb') as f:
-            content = f.read()
-        headers = [('X-Cache', 'HIT')]
+    if os.path.exists(cache_path):
+        content, headers = fetch_from_cache(url_path, cache_path)
     else:
-        print(f'X-Cache miss: {url_path} - fetching from {origin}{url_path}')
+        try:
+            content, headers = fetch_from_server(url_path, origin)
+        except TypeError:
+            print('error: Nothing to save in cache storage')
+        else:
+            save_in_cache(cache_path, content)
+    
+        request_handler.send_response(200)
+    try:
+        for key, value in headers:
+            request_handler.send_header(key, value)
+        request_handler.end_headers()
+        request_handler.wfile.write(content)
+    except UnboundLocalError:
+        print('error: Empty response')
+
+
+def fetch_from_cache(url_path: str, cache_path: str):
+    print(f'X-Cache hit: {url_path} - fetching from cache')
+    with open(cache_path, 'rb') as f:
+        content = f.read()
+    headers = [('X-Cache', 'HIT')]
+    return content, headers
+
+
+def fetch_from_server(url_path: str, origin: str):
+    print(f'X-Cache miss: {url_path} - fetching from {origin}{url_path}')
+    try:
         url = urlparse(origin)
         conn = HTTPConnection(url.netloc)
         conn.request('GET', url_path)
@@ -45,14 +69,16 @@ def handle_request(request_handler: SimpleHTTPRequestHandler, origin: str):
         headers = response.getheaders()
         headers.append(('X-Cache', 'MISS'))
         conn.close()
-        with open(cache_file_path, 'wb') as f:
-            f.write(content)
+        return content, headers
+    except InvalidURL:
+        print(f'error: Invalid URL {origin}{url_path}')
+    except:
+        print(f'error: Could not fetch from {origin}{url_path}')
 
-    request_handler.send_response(200)
-    for key, value in headers:
-        request_handler.send_header(key, value)
-    request_handler.end_headers()
-    request_handler.wfile.write(content)
+
+def save_in_cache(cache_path: str, content: bytes):
+    with open(cache_path, 'wb') as f:
+        f.write(content)
 
 
 def get_cache_file_path(url_path: str):
